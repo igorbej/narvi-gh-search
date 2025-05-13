774dfc672d1e4c380c87ae5bad3b585f91a28c1a
Igor Bejnarowicz <igor.bejnarowicz@gmail.com>
2025-05-13 11:37:50 +0200

    Use `CssBaseline` component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import CssBaseline from "@mui/material/CssBaseline";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
@@ -8,6 +9,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <CssBaseline />
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
