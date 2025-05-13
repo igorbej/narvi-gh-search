43af8410150e957b8332598addc4b2873b376670
Igor Bejnarowicz <igor.bejnarowicz@gmail.com>
2025-05-13 19:06:47 +0200

    Replace project's font & preload font file to avoid FOUT

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,28 @@
+import "@fontsource-variable/jetbrains-mono/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./index.css";
 
+const theme = createTheme({
+  typography: {
+    fontFamily: "'JetBrains Mono Variable', monospace",
+  },
+});
+
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <CssBaseline />
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 );
